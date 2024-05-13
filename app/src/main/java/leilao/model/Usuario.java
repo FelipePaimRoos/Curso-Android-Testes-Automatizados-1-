8000f874e14cd2f1d25857f7973e3d69d8f1b699
Felipe Paim Roos <felipepaimroos@gmail.com>
2024-05-13 19:16:05 -0300

    Implementando a Primeira feature

---
+++ app/src/main/java/leilao/model/Usuario.java
@@ -0,0 +1,13 @@
+package leilao.model;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+
+    private final String nome;
+
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+
+}
